1748f370ce9e6296f1df24067d6cf213ef5eb099
Sandip Gyawali <sandipgyawali10@gmail.com>
2024-11-04 23:33:59 +0545

    configuration

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+// import { defineConfig } from 'drizzle-kit';
+
+// drizzle config setup
+// export default defineConfig({
+//   schema: './apps/server/src/model/schema.ts',
+//   out: './drizzle',
+//   dialect: 'postgresql',
+//   dbCredentials: {
+//     url: process.env.DATABASE_URL ?? '',
+//   },
+//   verbose: process.env.NODE_ENV === 'development',
+//   strict: true,
+// });
